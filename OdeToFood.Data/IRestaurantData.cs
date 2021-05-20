cb6739571a89e60ed8f244f6f3305ea91a5f8e00
aaltukhov <altukhov.artem.dev@gmail.com>
2021-05-20 23:05:31 +0300

    adds new Data project

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+	public interface IRestaurantData
+	{
+		IEnumerable<Restaurant> GetAll();
+	}
+
+	public class InMemoryRestaurantData : IRestaurantData
+	{
+		List<Restaurant> restaurants;
+		public InMemoryRestaurantData()
+		{
+			restaurants = new List<Restaurant>
+			{ 
+				new Restaurant {Id = 1, Name = "First", Location = "Moscow", Cuisine = CuisineType.Indian},
+				new Restaurant {Id = 1, Name = "Second", Location = "Montreal", Cuisine = CuisineType.Italian},
+				new Restaurant {Id = 1, Name = "Third", Location = "Qwefv", Cuisine = CuisineType.Mexican}
+			};
+		}
+
+		public IEnumerable<Restaurant> GetAll()
+		{
+			return from r in restaurants
+				   orderby r.Name
+				   select r;
+		}
+	}
+
+}
